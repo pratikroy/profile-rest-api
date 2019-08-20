72ddc91fb48e4b042f2071b1a0b6d1edbe5b8dc7
pratikroy <pratikroy2206@gmail.com>
2019-08-21 01:22:08 +0530

    Temporary commit. Going to change it soon.

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from profiles_api import serializers
 from rest_framework import viewsets
+from profiles_api import models
 
 class HelloApiView(APIView):
     """Testing API View"""
@@ -90,3 +91,10 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy(self, request, pk=None):
         """handle deleting an object"""
         return Response({"http_method":"DELETE"})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
